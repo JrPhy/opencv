344910023ad4163f6da36d91826cff1990773634
JrPhy <j0958322080@gmail.com>
2019-07-10 09:31:25 +0800

    Create  lane_detection.cpp

---
+++ lane detection/lane_detection.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cmath>
+#include <opencv2\highgui\highgui.hpp>
+#include <opencv2\opencv.hpp>
+
+int main()
+{
+    cv::Mat frame, gray, dst, roi;
+    cv::VideoCapture cap("test1.mp4");
+    while(1)
+    {
+        cap >> frame;
+        if (frame.empty())
+            break;
+        // Display the resulting frame
+        cv::cvtColor(frame, gray, cv::COLOR_BGR2GRAY);
+        cv::Canny(gray, dst, 50, 500);
+        for(int i=0; i<0.5*dst.rows; i++) for(int j=0; j<dst.cols; j++) dst.at<uchar>(i,j) = 0;// setting ROI
+        std::vector<cv::Vec4i> lines;
+        // detect the lines
+        HoughLinesP(dst, lines, 1, CV_PI/180, 50, 30, 10 );
+        for( size_t i = 0; i < lines.size(); i++ )
+        {
+            cv::Vec4i l = lines[i];
+            if(fabs((float)(l[3]-l[1])/(float)(l[2]-l[0])) > 0.3)
+                cv::line( frame, cv::Point(l[0], l[1]), cv::Point(l[2], l[3]), cv::Scalar(255,0,0), 3, cv::LINE_AA, 0);
+        }
+        // Press  ESC on keyboard to exit
+        cv::namedWindow( "Source", cv::WINDOW_AUTOSIZE);
+        cv::imshow( "Source", frame );
+        char c = (char) cv::waitKey(25);
+        if(c==27)
+            break;
+    }
+    cap.release();// When everything done, release the video capture object
+    cv::destroyAllWindows();// Closes all the frames
+    return 0;
+}
