1fd8acea3ebb69140a32d7ea4d990788e097e329
JrPhy <j0958322080@gmail.com>
2019-07-16 16:25:25 +0800

    Update lane_detection.cpp

--- lane detection/lane_detection.cpp
+++ lane detection/lane_detection.cpp
@@ -6,7 +6,7 @@
 int main()
 {
     cv::Mat frame, gray, dst, roi;
-    cv::VideoCapture cap("test1.mp4");
+    cv::VideoCapture cap("night.mp4");
     while(1)
     {
         cap >> frame;
@@ -14,21 +14,27 @@ int main()
             break;
         // Display the resulting frame
         cv::cvtColor(frame, gray, cv::COLOR_BGR2GRAY);
+        cv::GaussianBlur(gray,gray,cv::Size(3, 3),0);
         cv::Canny(gray, dst, 50, 500);
-        for(int i=0; i<0.5*dst.rows; i++) for(int j=0; j<dst.cols; j++) dst.at<uchar>(i,j) = 0;// setting ROI
+        for(int i=0; i<0.6*dst.rows; i++) for(int j=0; j<dst.cols; j++) dst.at<uchar>(i,j) = 0;// setting ROI
         std::vector<cv::Vec4i> lines;
         // detect the lines
         HoughLinesP(dst, lines, 1, CV_PI/180, 50, 30, 10 );
         for( size_t i = 0; i < lines.size(); i++ )
         {
             cv::Vec4i l = lines[i];
-            if(fabs((float)(l[3]-l[1])/(float)(l[2]-l[0])) > 0.3)
-                cv::line( frame, cv::Point(l[0], l[1]), cv::Point(l[2], l[3]), cv::Scalar(255,0,0), 3, cv::LINE_AA, 0);
+            float m = fabs((float)(l[3]-l[1])/(float)(l[2]-l[0]));
+            if( m > 0.3)
+            {
+                if( m > 1.3 ) cv::line( frame, cv::Point(l[0], l[1]), cv::Point(l[2], l[3]), cv::Scalar(0,0,255), 3, cv::LINE_AA, 0);
+                else cv::line( frame, cv::Point(l[0], l[1]), cv::Point(l[2], l[3]), cv::Scalar(255,0,0), 3, cv::LINE_AA, 0);
+            }
+
         }
         // Press  ESC on keyboard to exit
         cv::namedWindow( "Source", cv::WINDOW_AUTOSIZE);
         cv::imshow( "Source", frame );
-        char c = (char) cv::waitKey(25);
+        char c = (char) cv::waitKey(1);
         if(c==27)
             break;
     }
